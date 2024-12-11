f43d171b4abf62d8307be98b014d4a0c564f6add
Chenhongliang777 <hl050708@qq.com>
2024-12-11 23:03:50 +0800

    Add files via upload

--- Monster.cpp
+++ Monster.cpp
@@ -232,3 +232,79 @@ void Star::moveToSequence(const std::vector<Vec2>& positions)
     // 将动画动作应用到精灵对象上
     this->runAction(Spawn::create(animate, sequence, nullptr)); // 执行序列动作
 }
+
+bool Boss::init() {
+    if (!Sprite::init()) {
+        return false;
+    }
+
+    //在怪物身上添加一个不可见按钮，用于实现集火
+    ui::Button* BUTTON = ui::Button::create("botton.png");
+    BUTTON->setOpacity(0);
+    BUTTON->setContentSize(Size(this->getContentSize().width, this->getContentSize().height));
+    BUTTON->addClickEventListener([this](Ref* sender) {
+        // 处理按钮点击事件
+        setAllTarget();
+        });
+    BUTTON->setPosition(Vec2(this->getContentSize().width / 2, this->getContentSize().height / 2));
+    addChild(BUTTON);
+
+    // 初始化HP
+    maxHP = HP = 1000;
+    // 初始化击杀奖励
+    value = 50;
+    // 初始化速度
+    speed = 100;
+    // 初始化半径
+    radius = 100;
+    //初始化伤害
+    hurt = 10;
+
+    return true;
+}
+
+void Boss::moveToSequence(const std::vector<Vec2>& positions)
+{
+    this->stopAllActions(); // 停止当前进行中的动作
+
+    Vector<FiniteTimeAction*> actions; // 创建一个动作数组
+    Vec2 thisPosition = this->getPosition();
+    for (Vec2 position : positions)
+    {
+        auto distance = position - thisPosition; // 计算移动距离
+        thisPosition = position;
+        auto duration = distance.length() / speed; // 计算移动时间
+        auto move = MoveTo::create(duration, position); // 创建移动动作
+        actions.pushBack(move); // 将移动动作添加到数组中
+    }
+
+    auto attacked = CallFunc::create([=]() {
+        auto target = this->getParent();
+        auto Scene = dynamic_cast<SceneBase*>(target);
+        Scene->getCarrot()->takeDamage(hurt);//根据怪物的伤害对萝卜造成伤害
+        this->setHP(0);//攻击后怪物死亡
+
+        });
+    auto sequenceAction = Sequence::create(actions); // 创建序列动作
+    auto sequence = Sequence::create(sequenceAction, attacked, nullptr);
+
+    // 创建一个空的动画对象
+    auto animation = Animation::create();
+    // 添加动画帧
+    for (int i = 1; i <= 2; i++) {
+        char filename[100];
+        sprintf(filename, "Boss%d.png", i);
+        auto frame = SpriteFrame::create(filename, Rect(0, 0, 200, 200));
+        animation->addSpriteFrame(frame);
+    }
+
+    // 设置动画属性
+    animation->setDelayPerUnit(0.1f);//nf=每隔n秒播放一帧
+    animation->setLoops(-1);//让动画一直循坏播放
+
+    // 创建动画动作对象
+    auto animate = Animate::create(animation);
+
+    // 将动画动作应用到精灵对象上
+    this->runAction(Spawn::create(animate, sequence, nullptr)); // 执行序列动作
+}
